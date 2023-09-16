b8b6634d4b5e787d698db359fbd14d0ebfd5c4d9
Ankur <ankurchanda198@gmail.com>
2023-09-16 22:28:08 +0530

    Added home page and user login

--- index.php
+++ index.php
@@ -13,6 +13,16 @@
     <link rel="stylesheet" type="text/css" href="css\styles.css">
 </head>
 <body>
-    
+    <div class="row">
+        <div class="container text-center">
+            <img src="includes\logo.jpg" class="rounded-image">
+        </div>
+        <div class="col-md-4 m-auto" id="home_page">
+            <center><h3>Choose login role ?</h3></center><br>
+            <a href="user_login.php" class="btn btn-success" style="margin-right: 20px;">User Login</a>
+            <a href="register.php" class="btn btn-warning" style="margin-right: 20px;">User Registration</a>
+            <a href="admin\admin_login.php" class="btn btn-info">Admin Login</a>
+        </div>
+    </div>
 </body>
 </html>
---
+++ user_login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TaskEase</title>
+    <!-- jQuery file -->
+    <script src="includes\jquery_latest.js"></script>
+    <!-- Bootstrap files -->
+    <link rel="stylesheet" type="text/css" href="bootstrap\css\bootstrap.min.css">
+    <script src="bootstrap\js\bootstrap.min.js"></script>
+    <!-- External CSS files -->
+    <link rel="stylesheet" type="text/css" href="css\styles.css">
+</head>
+<body>
+    <div class="row">
+        <div class="col-md-3 m-auto" id="login_home_page">
+            <form action="" method="post">
+                <div class="form-group">
+                    <input type="email" name="email" class="form-control" placeholder="Enter Email" required>
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" class="form-control" placeholder="Enter Password" required>
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="userLogin" value="Login" class="btn btn-warning">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
