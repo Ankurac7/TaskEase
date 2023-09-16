1daaa09cbfb4b5785b1aed70d77d703d5b822e33
Ankur <ankurchanda198@gmail.com>
2023-09-16 20:14:51 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TaskEase</title>
+    <!-- jQuery file -->
+    <script src="includes\jquery_latest.js"></script>
+    <!-- Bootstrap files -->
+    <link rel="stylesheet" type="text/css" href="bootstrap\css\bootstrap.min.css">
+    <script src="bootstrap\js\bootstrap.min.js"></script>
+    <!-- External CSS files -->
+    <link rel="stylesheet" type="text/css" href="css\styles.css">
+</head>
+<body>
+    
+</body>
+</html>
