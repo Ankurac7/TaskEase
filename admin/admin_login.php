514dad2e020a82b3acf861f1821a4603c66ec71d
Ankur <ankurchanda198@gmail.com>
2023-09-17 01:00:44 +0530

    Connected database and register users

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -3,19 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Admin Login Page</title>
     <!-- jQuery file -->
-    <script src="includes\jquery_latest.js"></script>
+    <script src="..\includes\jquery_latest.js"></script>
     <!-- Bootstrap files -->
-    <link rel="stylesheet" type="text/css" href="bootstrap\css\bootstrap.min.css">
-    <script src="bootstrap\js\bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="..\bootstrap\css\bootstrap.min.css">
+    <script src="..\bootstrap\js\bootstrap.min.js"></script>
     <!-- External CSS files -->
-    <link rel="stylesheet" type="text/css" href="css\styles.css">
+    <link rel="stylesheet" type="text/css" href="..\css\styles.css">
 </head>
 <body>
     <div class="row">
         <div class="col-md-3 m-auto" id="login_home_page">
-            <center><h3 style="background-color: #5A8F7B;padding: 5px; width: 15vw;">User Login</h3></center>
+            <center><h3 style="background-color: #5A8F7B;padding: 5px; width: 15vw;">Admin Login</h3></center>
             <form action="" method="post">
                 <div class="form-group">
                     <input type="email" name="email" class="form-control" placeholder="Enter Email" required>
@@ -27,7 +27,7 @@
                     <center><input type="submit" name="userLogin" value="Login" class="btn btn-warning"></center>
                 </div>
             </form>
-            <center><a href="index.php" class="btn btn-danger">Go to Home</a></center>
+            <center><a href="..\index.php" class="btn btn-danger">Go to Home</a></center>
         </div>
     </div>
 </body>
