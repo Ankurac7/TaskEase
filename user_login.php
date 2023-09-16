178a63b22197f7ffc4dccb8adbf9a49b55699321
Ankur <ankurchanda198@gmail.com>
2023-09-17 01:50:26 +0530

    Integrated login with database and register

--- user_login.php
+++ user_login.php
@@ -1,3 +1,23 @@
+<?php
+    include('includes\connection.php');
+    if(isset($_POST['userLogin'])){
+        $query = "select email,password,name,uid from users where email = '$_POST[email]' AND password = '$_POST[password]'";
+        $query_run = mysqli_query($connection,$query);
+        if (mysqli_num_rows($query_run)){
+            echo "<script type='text/javascript'>
+            window.location.href = 'user_dashboard.php';
+            </script>
+            ";
+        }
+        else{
+            echo "<script type='text/javascript'>
+            alert('Please enter correct Email Id and Password.');
+            window.location.href = 'user_login.php';
+            </script>
+            ";
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
